f7e1dbc657d5d0bcb71fce79004f27204952aaff
Margarita <rita.akhmadulina@gmail.com>
2016-05-17 15:00:19 +0300

    add sections

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
 //=include lib/jquery.js
-//=include lib/swiper.jquery.min.js
+//=include lib/slick.min.js
+//=include lib/jquery.mb.YTPlayer.min.js
 //=include common.js
--- src/js/common.js
+++ src/js/common.js
@@ -1,19 +1,5 @@
 $(document).ready(function() {
 
-    function swiperSlider () {
-
-        var swiper = new Swiper('.swiper-container', {
-            nextButton: '.swiper-button-next',
-            prevButton: '.swiper-button-prev',
-            //paginationClickable: true,
-            //pagination: '.swiper-pagination',
-            slidesPerView: 1,
-            keyboardControl: true,
-            loop: true
-        })
-
-    }
-
-    swiperSlider();
+    $('.player').mb_YTPlayer();
 
 });
