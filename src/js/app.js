484fcd38d63712ba51097692ec2ce84e323c6644
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2016-06-17 14:47:52 +0300

    gallery add

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 //=include lib/jquery.js
 //=include lib/slick.min.js
+//=include lib/jquery.fancybox.js
 //=include lib/jquery.mb.YTPlayer.min.js
 //=include common.js
--- src/js/common.js
+++ src/js/common.js
@@ -2,4 +2,17 @@ $(document).ready(function() {
 
     $('.player').mb_YTPlayer();
 
+    //fancybox
+    $(".img-gallery").fancybox({
+        padding : 0,
+        //openEffect	: 'none',
+        //closeEffect	: 'none',
+        helpers: {
+            overlay: {
+                locked: false
+            }
+        }
+    });
+
+
 });
