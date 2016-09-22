591ee88d740f6ea8596c862f0bfd046dd4f566a8
Rita Akhmadulina <rita.akhmadulina@gmail.com>
2016-09-22 16:18:38 +0300

    nav-menu fix

--- build/js/common.js
+++ build/js/common.js
@@ -24,11 +24,27 @@ $(document).ready(function() {
         $('.nav-mob:visible').click();
     });
 
-
+    // btn show-more
     $('#show-more').click(function() {
         $('#more-works').fadeIn();
         $(this).hide();
     });
+
+    // nav
+    $(".nav").on("click","a", function (event) {
+        //отменяем стандартную обработку нажатия по ссылке
+        event.preventDefault();
+        //забираем идентификатор бока с атрибута href
+        var id  = $(this).attr('href'),
+
+        //узнаем высоту от начала страницы до блока на который ссылается якорь
+        top = $(id).offset().top;
+        margin = 140;
+
+        //анимируем переход на расстояние - top за 1500 мс
+        $('body,html').animate({scrollTop: top - margin}, 1000);
+    });
+
 
 
 });
